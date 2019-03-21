08f827564e99194e1b7ba1e97a21622fb41555ec
Andy <andy@tests-MacBook-Pro.local>
2019-03-21 14:08:58 +0800

    Updates

---
+++ precache-manifest.990ca1fbe0f3c244df37081ca545159c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "528f535a409e9fdfe1d4",
+    "url": "/static/js/main.db780497.chunk.js"
+  },
+  {
+    "revision": "6d975708dc134e2d6d6c",
+    "url": "/static/js/2.78fdae8b.chunk.js"
+  },
+  {
+    "revision": "528f535a409e9fdfe1d4",
+    "url": "/static/css/main.51390d01.chunk.css"
+  },
+  {
+    "revision": "6d975708dc134e2d6d6c",
+    "url": "/static/css/2.e448490b.chunk.css"
+  },
+  {
+    "revision": "5a2c7db7500c9f902522fbb0eed80d54",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f55a7c2984e09e34e465e4394c9d72bc.js"
+  "/precache-manifest.990ca1fbe0f3c244df37081ca545159c.js"
 );
 
 workbox.clientsClaim();
